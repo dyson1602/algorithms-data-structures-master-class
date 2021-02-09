74fa46ce2a2e2e6e3f35a446555be81f91298187
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-09 17:18:08 -0500

    countUniqueValues solution with while loop

---
+++ algorithms/countUniqueValues.js
@@ -0,0 +1,32 @@
+//Implement a function called countUniqueValues which accepts a sorted array, and counts the unique
+//values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+
+const arr1 = [1, 1, 1, 1, 1, 2] //2
+const arr2 = [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13] //7
+const arr3 = [] //0
+const arr4 = [-2, -1, -1, 0, 1] //4
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0
+  }
+  let left = 0
+  let right = 1
+
+  while (right < arr.length) {
+    if (arr[left] === arr[right]) {
+      right++
+    } else {
+      left++
+      arr[left] = arr[right]
+      right++
+    }
+  }
+  return (left + 1)
+}
+
+console.log(countUniqueValues(arr1)) //2
+console.log(countUniqueValues(arr2)) //7
+console.log(countUniqueValues(arr3)) //0
+console.log(countUniqueValues(arr4)) //4
--- algorithms/sumZero.js
+++ algorithms/sumZero.js
@@ -1,5 +1,5 @@
 //Write a function called sumZero which accepts a sorted array of integers. 
-//The function should find the first paid where the sum is 0. Return an array 
+//The function should find the first pair where the sum is 0. Return an array 
 //that includes both the values that sum to zero or undefined if a pair does not exist.
 
 let arr1 = [-3, -2, -1, 0, 1, 2, 3] //[-3,3]
