6f55bb9958c0ea5e17eb2f28fb2e6f3b504541c8
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-22 15:34:33 -0500

    bubble sort with break conditional

---
+++ algorithms/sorting/bubbleSort/bubbleSort.js
@@ -0,0 +1,27 @@
+//Largest value bubbles to the top
+
+
+
+//bubblesort
+//Time: Best-case O(n), Worst-case O(n^2)
+function bubbleSort(array) {
+  const swap = (arr, ind1, ind2) => {
+    [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]
+  }
+  let noSwaps
+  for (let i = 0; i < array.length; i++) {
+    noSwaps = true
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1)
+        noSwaps = false
+      }
+    }
+    if (noSwaps) break
+  }
+  return array
+}
+
+let array = [2, 6, 8, 3, 6, 43, 8, 56, 23, 65, 853, 25, 21]
+
+console.log(bubbleSort(array))
