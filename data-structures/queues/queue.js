9b5f690ace4898cbb06a2e873815f01d4ea775d7
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-25 14:15:01 -0500

    add: .enqueue() and .dequeue() to Queue

--- data-structures/queues/queue.js
+++ data-structures/queues/queue.js
@@ -1,14 +1,45 @@
+//Time: O(1) for insertion and removal. Searching and accessing shouldn't use a
+//queue. It is basicaly a special type of linked list.
+
 class Node {
-  constructor(val){
+  constructor(val) {
     this.val = val
     this.next = null
   }
 }
 
 class Queue {
-  constructor(){
+  constructor() {
     this.first = null
     this.last = null
     this.size = 0
   }
+  enqueue(val) {
+    let newNode = new Node(val)
+    if (!this.first) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    return this.size++
+  }
+  dequeue() {
+    if (!this.first) return null
+    let temp = this.first
+    if (this.first === this.first) this.last = null
+    this.first = this.first.next
+    this.size--
+    return temp.val
+  }
 }
+
+let queue = new Queue()
+
+queue.enqueue(100)
+queue.enqueue(200)
+queue.enqueue(300)
+queue.enqueue(400)
+queue.enqueue(500)
+queue.enqueue(600)
