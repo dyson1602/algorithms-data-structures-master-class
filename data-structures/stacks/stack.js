3b538c4712fc96988b4fdb4cb2e44a969e074277
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-25 13:55:53 -0500

    add: .push() and .pop() to Stack class. fixed: this.next = null

--- data-structures/stacks/stack.js
+++ data-structures/stacks/stack.js
@@ -1,3 +1,5 @@
+//BIG O of Stacks
+
 class Node {
   constructor(val){
     this.val = val
@@ -11,4 +13,34 @@ class Stack {
     this.last = null
     this.size = 0
   }
+  push(val){
+    let newNode = new Node(val)
+    if(!this.first){
+      this.first = newNode
+      this.last = newNode
+    } else {
+      let temp = this.first
+      this.first = newNode
+      this.first.next = temp
+    }
+    return this.size++
+  }
+  pop(){
+    if(this.size = 0) return null
+    let temp = this.first
+    if(this.first === this.last){
+      this.last = null
+    }
+    this.first = this.first.next
+    this.size--
+    return temp.val
+  }
 }
+
+let stack = new Stack()
+
+stack.push(100)
+stack.push(200)
+stack.push(300)
+stack.push(400)
+stack.push(500)
