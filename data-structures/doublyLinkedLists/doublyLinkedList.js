7d69c7986c8feaabf4c385bbe91592151697497c
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-24 21:04:22 -0500

    add: .shift(), .unShift(), and added returns to .pop() and .push()

--- data-structures/doublyLinkedLists/doublyLinkedList.js
+++ data-structures/doublyLinkedLists/doublyLinkedList.js
@@ -23,6 +23,7 @@ class DoublyLinkedList {
       this.tail = newNode
     }
     this.length++
+    return this
   }
   pop() {
     if (!this.head) return undefined
@@ -35,5 +36,35 @@ class DoublyLinkedList {
       this.tail.next = null
       poppedNode.prev = null
     }
+    this.length--
+    return poppedNode
   }
+  shift(){
+    if(this.length === 0) return undefined
+    let oldHead = this.head
+    if(this.length === 1){
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = oldHead.next
+      this.head.prev = null
+      oldHead.next = null
+    }
+    this.length--
+    return oldHead
+  }
+  unshift(val){
+    let newNode = new Node(val)
+    if(!this.head){
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.head.prev = newNode
+      newNode.next = this.head
+      this.head = newNode
+    }
+    this.length++
+    return this
+  }
+
 }
