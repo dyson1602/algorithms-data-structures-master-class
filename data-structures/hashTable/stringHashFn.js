666eb674ab54ee7782b1227e30d03acf7c578e1f
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-27 15:03:10 -0500

    add: hashTwo function, includes prime number and limitation on length of string so it limits iterations to 100

--- data-structures/hashTable/stringHashFn.js
+++ data-structures/hashTable/stringHashFn.js
@@ -10,13 +10,29 @@
 
 
 //Problems with this hash: only hashes strings, not constant time, clustered data
-function hash(key, arrayLen){
+function hash(key, arrayLen) {
   let total = 0
-  for (let char of key){
+  for (let char of key) {
     let value = char.charCodeAt(0) - 96
     total = (total + value) % arrayLen
   }
   return total
 }
 
-console.log(hash("maroon",10))
+//Slight improvement on performance and randomness
+
+function hashTwo(key, arrayLen) {
+  let total = 0
+  let WEIRD_PRIME = 31
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i]
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen
+  }
+  return total
+}
+
+console.log(hashTwo("maroon", 13))
+console.log(hashTwo("black", 13))
+console.log(hashTwo("red", 13))
+console.log(hashTwo("cyan", 13))
