69e6294805e0620f2e811fa6be8a65348b0d3c1f
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-28 10:16:40 -0500

    add: .removeEdge() and .removeVertex() added to Graph class

--- data-structures/graphs/adjacencyList.js
+++ data-structures/graphs/adjacencyList.js
@@ -1,3 +1,5 @@
+//undirected, unweighted graph with adjacency list
+
 class Graph {
   constructor() {
     this.adjacencyList = {}
@@ -5,12 +7,25 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
   }
-  addEdge(v1, v2){
-    if(!this.adjacencyList[v1].includes(v2)){
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1].includes(v2)) {
       this.adjacencyList[v1].push(v2)
     }
-    if(!this.adjacencyList[v2].includes(v1)){
-       this.adjacencyList[v2].push(v1)
+    if (!this.adjacencyList[v2].includes(v1)) {
+      this.adjacencyList[v2].push(v1)
+    }
+  }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2)
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1)
+  }
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      while (this.adjacencyList[vertex].length) {
+        let adjacentVertex = this.adjacencyList[vertex].pop()
+        this.removeEdge(vertex, adjacentVertex)
+      }
+      delete this.adjacencyList[vertex]
     }
   }
 }
