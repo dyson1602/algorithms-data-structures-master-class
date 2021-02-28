6da1b52b8aa24a4cbecc80777efc25de864592f3
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-28 11:50:15 -0500

    add: .dfsRecursive() to Graph

--- data-structures/graphs/adjacencyList.js
+++ data-structures/graphs/adjacencyList.js
@@ -28,4 +28,40 @@ class Graph {
       delete this.adjacencyList[vertex]
     }
   }
+  dfsRecursive(start) {
+    const result = []
+    const visited = {}
+    const adjacencyList = this.adjacencyList
+  
+    function dfs(vertex) {
+      if (!vertex) return null
+        visited[vertex] = true
+        result.push(vertex)
+        adjacencyList[vertex].forEach(neighbor => {
+          if(!visited[neighbor]){
+            return dfs(neighbor)
+          }
+        })
+    }
+
+    dfs(start)
+    return result
+  }
 }
+
+let g = new Graph()
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
