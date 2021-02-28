965f008da29d9c60486af679c2b9cd88de32f8dd
dyson1602 <cclark000@citymail.cuny.edu>
2021-02-28 10:02:16 -0500

    new: Graph class implemented with adjacency list

---
+++ data-structures/graphs/adjacencyList.js
@@ -0,0 +1,16 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+  }
+  addEdge(v1, v2){
+    if(!this.adjacencyList[v1].includes(v2)){
+      this.adjacencyList[v1].push(v2)
+    }
+    if(!this.adjacencyList[v2].includes(v1)){
+       this.adjacencyList[v2].push(v1)
+    }
+  }
+}
